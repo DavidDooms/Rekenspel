27c2d6498c7aaed7a83e84a279d6a3ea5e79de63
David Dooms <david.dooms.dd@gmail.com>
2024-11-19 20:58:50 +0100

    reworked calculation + inserted tkinter

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import numpy as np
+import tkinter as tk
+import tkinter.ttk as tkk
 
 end_game = False
+
+window = tk.Tk()
+window.geometry("100x100")
+
+greeting = tk.Label(text="HALLO ROXANE! \n\n WE GAAN REKENEN!", bg="pink")
+greeting.pack()
+
+start_button = tk.Button(window, text="START", command=window.destroy)
+start_button.pack(padx=5, pady=10)
+
+window = tk.Tk()
+window.geometry("100x100")
+
+window.mainloop()
 
 while not end_game:
     num1 = np.random.randint(11)
     num2 = np.random.randint(11)
 
-    sol = input(f"Hoeveel is {num1} + {num2}?")
+    sol = input(f"Hoeveel is {num1} + {num2}? Stoppen? (Q)")
 
-    while not (int(sol) == (num1 + num2)):
-        print(f"Fout, probeer opnieuw!")
-        sol = input(f"Hoeveel is {num1} + {num2}?")
+    if sol.lower() == "q":
+        end_game = True
+        print("Einde spelletje, goed gedaan!")
+    else:
+        while not (int(sol) == (num1 + num2)):
+            print(f"Fout, probeer opnieuw!")
+            sol = input(f"Hoeveel is {num1} + {num2}?")
 
     print("Goed bezig!")
 
-    stop = input("stoppen? (q)")
-    if stop.lower() == "q":
-        end_game = True
-        print("Einde spelletje, goed gedaan!")
+
